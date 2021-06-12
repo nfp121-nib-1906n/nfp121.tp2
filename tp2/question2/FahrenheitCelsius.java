687be9f7d92f0923745e4157497ac8829ac8d16a
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-06-12 19:15:47 +0300

    Update FahrenheitCelsius.java

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -1,38 +1,48 @@
 package question2;
 
-
 /**
- * DÈcrivez votre classe FahrenheitCelsius ici.
+ * D√©crivez votre classe FahrenheitCelsius ici.
  * 
- * @author (votre nom) 
- * @version (un numÈro de version ou une date)
+ * @author Maria Bou Aoun
+ 
  */
 public class FahrenheitCelsius{
 
-     /** le point d'entrÈe de cette application, 
-      * dont le commentaire est ‡ complÈter
-      *
-      *  @param args ...
-      */
-     public static void main(String[] args){
-       try{
-       
-      
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
-       }
-       
-     }
-     
-     /** 
-      * la mÈthode ‡ complÈter. 
-      *   @param f la valeur en degrÈ Fahrenheit
-      *   @return  la conversion en degrÈ Celsius
-      */
-     public static float fahrenheitEnCelsius( int f){
-       // ...
-       return 0.F;	// ‡ complÈter	en remplaÁant la valeur retournÈe par la fonction de conversion
-       // ...
-     }
+    /** le point d'entr√©e de cette application, 
+     * dont le commentaire est √† compl√©ter
+     *
+     *  @param args ...
+     */
+    public static void main(String[] args){
+        int fahrenheit = 0;
+        double celsius = 0.0;
+
+        for (String fahrenheitTemp : args) {
+            try {
+                fahrenheit = Integer.parseInt(fahrenheitTemp);
+                celsius = fahrenheitEnCelsius(fahrenheit);
+
+                System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); 
+
+            } catch (NumberFormatException nfe){
+                System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
+            }
+        }   
+
+    }
+
+    /** 
+     * la m√©thode √† compl√©ter. 
+     *   @param f la valeur en degr√© Fahrenheit
+     *   @return  la conversion en degr√© Celsius
+     */
+
+   
+    public static double fahrenheitEnCelsius(int f){
+        double celsiusTemp = (((double)5)/((double)9))*(f - 32);
+        celsiusTemp = ((long)(celsiusTemp * 10))/10.0;
+
+        return celsiusTemp;
+    }
 
 }
